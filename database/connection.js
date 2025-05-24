f9dc644191ca8ba77c04f5a579d79fddba6aedd1
abdul8704 <abdulaziz87049@gmail.com>
2025-05-24 21:03:33 +0530

    update mongoose

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 function connectDB(url){
   return mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
   });
 }
 
